2f47bf88722804b6aee5426efafa01083d1d4c8a
Rdsarkar <rajapscl@gmail.com>
2021-10-19 14:04:08 +0600

    gg

--- IMS/Startup.cs
+++ IMS/Startup.cs
@@ -48,6 +48,7 @@ namespace IMS
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "IMS v1"));
             }
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
             app.UseHttpsRedirection();
 
             app.UseRouting();
